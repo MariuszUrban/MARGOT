be48fc814cc9da28174e4b5582d7b7563af8c096
Mariusz <urban.mariusz@icloud.com>
2020-09-06 14:42:51 +0200

    master

--- margot/src/js/components/Contact/index.jsx
+++ margot/src/js/components/Contact/index.jsx
@@ -1,39 +1,82 @@
-import React from "react";
 import "../../../sass/_contact/_index.scss";
+import emailjs from "emailjs-com";
 
-export default function index() {
-  return (
-    <div className="contactWrap">
-      <div className="contactWrapHeader">
-        <h1>
-          Masz pytania? Szukasz pracownika?
-        </h1>
-        <span> Napisz do mnie wiadomość! </span>
-      </div>
+import React, { Component } from "react";
 
-      <div className="contactWrapForm">
-      <div className='space col-lg-1'></div>
-        <div className="contactIMG col-lg-4">
-          <img src="https://i.ibb.co/NZV0L7w/question.png" alt="question" />
+export default class index extends Component {
+
+  sendMail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "mail_service",
+        "template_yngrepp",
+        e.target,
+        "user_TW6NNvRcLgA44h8jaLHXr"
+      )
+      .then(
+        (result) => {
+          console.log("result", result);
+        },
+        (error) => {
+          console.log("error", error);
+        }
+      );   
+  };
+
+  render() {
+    return (
+      <div className="contactWrap">
+        <div className="contactWrapHeader">
+          <h1>Masz pytania? Szukasz pracownika?</h1>
+          <span> Napisz do mnie wiadomość! </span>
         </div>
-        <div  className='space col-lg-2'></div>
-        <div className="contactFormMSG  col-lg-4">
-          <form>
-            <div className='inputs'>
-              <label>Imię</label>
-              <input />
-              <label>E-Mail</label>
-              <input />
-            </div>
-            <div className='message'>
-              <label>Wiadomość</label>
-              <input />
-              <button>Wyślij</button>
-            </div>
-          </form>
+
+        <div className="contactWrapForm">
+          <div className="space col-lg-1"></div>
+          <div className="contactIMG col-lg-4">
+            <img
+              src="https://i.ibb.co/M6gYt60/question-small.png"
+              alt="question-small"
+            />{" "}
+          </div>
+          <div className="contactFormMSG  col-lg-4">
+            <form id="msg-form" className="msg-flex" onSubmit={this.sendMail}>
+              <div className="inputs">
+                <div className="single">
+                  <div className="label">
+                    <label>Imię</label>
+                  </div>
+                  <div className="input" >
+                    <input name='name'/>
+                  </div>
+                </div>
+                <div className="single">
+                  <div className="label" >
+                    <label>E-mail</label>
+                  </div>
+                  <div className="input">
+                    <input type="email" name='email'/>
+                  </div>
+                </div>
+              </div>
+              <div className="message">
+                <div className="messageTitle">
+                  <label>Wiadomość</label>
+                </div>
+                <div className="messageInput">
+                  
+                  <textarea name='message'/>
+                </div>
+                <div className="messageBTN">
+                  <button type='submit' form='msg-form'>Wyślij !</button>
+                </div>
+              </div>
+            </form>
+          </div>
+          <div className="space col-lg-1"></div>
         </div>
-      <div  className='space col-lg-1'></div>
       </div>
-    </div>
-  );
+    );
+  }
 }
